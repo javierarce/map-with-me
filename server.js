e21b190368031777300771fea8feb267a6315c74
Javier Arce <javierarce@users.noreply.github.com>
2020-06-06 19:30:32 +0200

    Organizes code

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const rss = require('rss')
 const sanitize = require('sanitize-html')
 const helmet = require('helmet')
 
+const Mapper = require('./lib/map')
 const Storage = require('./lib/db')
+
 const DB = new Storage()
+const Map = new Mapper(DB)
 
 const passport = require('passport')
 const TwitterStrategy = require('passport-twitter').Strategy
@@ -43,199 +46,17 @@ if (devServerEnabled) {
   //timeout=1000:Time from disconnecting from server to reconnecting
   webpackConfig.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000')
 
-  //Add HMR plugin
   webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
 
   const compiler = webpack(webpackConfig)
 
-  //Enable "webpack-dev-middleware"
   app.use(webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath
   }))
 
-  //Enable "webpack-hot-middleware"
   app.use(webpackHotMiddleware(compiler))
 }
 
-const onSave = (request, response) =>  {
-  let lat = request.body.coordinates.lat
-  let lng = request.body.coordinates.lng
-  let zoom = request.body.zoom
-
-
-  let description = sanitize(request.body.description)
-  let name = sanitize(request.body.name)
-  let address = sanitize(request.body.address)
-
-  request.session.coordinates = { lat, lng, zoom, name, description, address }
-
-  response.json({ lat, lng, zoom, description, name, address })
-}
-
-const removeSession = (request) => {
-  request.session.coordinates = null
-  delete request.session.coordinates
-  request.session.coordinates = undefined
-}
-
-const onRemoveSession = (request, response) =>  {
-  removeSession(request)
-  response.json({ user: request.user, coordinates: request.session.coordinates })
-}
-
-const onRejectLocation = (request, response) =>  {
-  let id = request.body.id 
-  let username = request.session.passport.user.username
-
-  if (username !== config.ADMIN.ADMIN_USERNAME) {
-    response.json({ error: 'Unauthorized' }) 
-    return
-  }
-
-  DB.rejectLocation({ id }).then((result) => { 
-    response.json(result) 
-  }).catch((e) => {
-    response.json({ error: e }) 
-  })
-}
-
-const onApproveLocation = (request, response) =>  {
-  let id = request.body.id 
-  let username = request.session.passport.user.username
-
-  if (username !== config.ADMIN.ADMIN_USERNAME) {
-    response.json({ error: 'Unauthorized' }) 
-    return
-  }
-
-  DB.approveLocation({ id }).then((result) => { 
-    response.json(result) 
-  }).catch((e) => {
-    response.json({ error: e }) 
-  })
-}
-
-const onRemoveLocation = (request, response) =>  {
-  let location = request.body.location
-  let id = location.id
-  let username = request.session.passport.user.username
-
-  if (username === location.user.username || username === config.ADMIN.ADMIN_USERNAME) {
-    DB.removeLocation({ id }).then((result) => { 
-      response.json({ id }) 
-    }).catch((e) => {
-      response.json({ error: e }) 
-    })
-  } else {
-    response.json({ error: 'Unauthorized' }) 
-  }
-}
-
-const onAddLocation = (request, response) =>  {
-
-  removeSession(request)
-
-  let username = undefined
-
-  if (request.session && request.session.passport) {
-    username = request.session.passport.user.username
-  }
-
-  let userId = request.user.id 
-  let lat = request.body.coordinates.lat
-  let lng = request.body.coordinates.lng
-
-  let address = sanitize(request.body.address)
-  let description = sanitize(request.body.description)
-  let name = sanitize(request.body.name)
-  let approved = !config.ADMIN.MODERATED || config.ADMIN.ADMIN_USERNAME ===  username
-
-  DB.createLocation({ userId, lng, lat, name, description, address, approved }).then((result) => { 
-    let location = result.dataValues
-    location.user = request.user
-    response.json(location) 
-  }).catch((e) => {
-    response.json({ error: e }) 
-  })
-}
-
-const onGetStatus = (request, response) =>  {
-  response.json({ user: request.user, coordinates: request.session.coordinates })
-}
-
-const onGetLocations = (request, response) =>  {
-
-  let username = undefined
-
-  if (request.session && request.session.passport) {
-    username = request.session.passport.user.username
-  }
-
-  let approved = config.ADMIN.MODERATED && (config.ADMIN.ADMIN_USERNAME !==  username)
-
-  DB.getLocations({ approved }).then((locations) => {
-    response.json({ locations })
-  }).catch((e) => {
-    response.json({ locations: [] })
-  })
-}
-
-const onGetCSV = (request, response) => {
-  DB.getLocations({ approved: true }).then((locations) => {
-
-    let csv = []
-
-    csv.push([ 'title', 'address', 'lat', 'lng', 'description', 'author', 'date' ].join(', '))
-
-    locations.forEach((location) => {
-
-      const title = `"${location.name}"`
-      const address = `"${location.address}"`
-      const lat = location.lat
-      const lng = location.lng
-      const description = `"${location.description}"`
-      const author = location.user.username
-      const date = new Date(location.updatedAt).toISOString()
-
-      csv.push([ title, address, lat, lng, description, author, date ].join(', '))
-    })
-
-    let date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').split(' ').join('_')
-    let filename = `map-with-me-${date}.csv`
-
-    response.setHeader('Content-Type', 'text/csv')
-    response.setHeader('Content-Disposition', `attachment; filename=${filename}`);
-    response.status(200).send(csv.join('\n'))
-  })
-}
-
-const onGetRSS = (request, response) => {
-  const domain = request.headers.host
-  const URL = `https://${domain}`
-
-  let feed = new rss({
-    title: `Map with Me @ ${config.TEXTS.MAIN_TITLE}`,
-    description: config.MAP.DEFAULT_SEARCH_LOCATION,
-    feed_url: `${URL}/rss`,
-    site_url: URL,
-    author: config.ADMIN.ADMIN_USERNAME
-  })
-
-  DB.getLocations({ approved: true }).then((locations) => {
-    locations.forEach((location) => {
-      const url = `${URL}/l/${location.id}`
-      const description = `${location.description} - ${location.address}`
-      const title = location.name
-      const author = location.user.username
-      const date = location.updatedAt
-
-      feed.item({ title, description, url, author, date })
-    })
-
-    response.setHeader('Content-Type', 'application/rss+xml')
-    response.send(feed.xml({ indent: true }))
-  })
-}
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
@@ -276,24 +97,28 @@ passport.use(new TwitterStrategy({
 
 app.use('/leaflet', express.static(__dirname + '/node_modules/leaflet/dist'))
 
-app.post('/api/add', onAddLocation)
-app.post('/api/remove', onRemoveLocation)
-app.post('/api/approve', onApproveLocation)
-app.post('/api/reject', onRejectLocation)
-app.post('/api/save', onSave)
+app.post('/api/add', (request, response) => { Map.onAddLocation(request, response)})
+app.post('/api/remove', (request, response) => { Map.onRemoveLocation(request, response)})
+app.post('/api/approve', (request, response) => { Map.onApproveLocation(request, response)})
+app.post('/api/reject', (request, response) => { Map.onRejectLocation(request, response)})
+app.post('/api/save', (request, response) => { Map.onSave(request, response)})
 
-app.get('/api/locations', onGetLocations)
-app.get('/api/status', onGetStatus)
-app.get('/api/reset', onRemoveSession)
+app.get('/api/locations', (request, response) => { Map.onGetLocations(request, response)})
+app.get('/api/status', (request, response) => { Map.onGetStatus(request, response)})
+app.get('/api/reset', (request, response) => { Map.onRemoveSession(request, response)})
 
-app.get('/rss', onGetRSS)
-app.get('/csv', onGetCSV)
+app.get('/rss', (request, response) => { Map.onGetRSS(request, response)})
+app.get('/csv', (request, response) => { Map.onGetCSV(request, response)})
 
 app.get('/auth/twitter', passport.authenticate('twitter'))
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }))
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html')
+  response.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/config', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html')
 })
 
 const listener = app.listen(process.env.PORT, function() {
