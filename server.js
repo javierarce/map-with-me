d143d1fc2acc267fdaac6ff3931c371875f5ae18
Javier Arce <javierarce@users.noreply.github.com>
2020-06-07 13:29:36 +0200

    Requires password to save configuration

--- server.js
+++ server.js
@@ -106,8 +106,8 @@ app.post('/api/approve', (request, response) => { Map.onApproveLocation(request,
 app.post('/api/reject', (request, response) => { Map.onRejectLocation(request, response)})
 app.post('/api/save', (request, response) => { Map.onSave(request, response)})
 
-app.get('/api/config', (request, response) => { Map.onGetConfig(request, response)})
 app.post('/api/config', (request, response) => { Map.onSaveConfig(request, response)})
+app.get('/api/config', (request, response) => { Map.onGetConfig(request, response)})
 
 app.get('/api/locations', (request, response) => { Map.onGetLocations(request, response)})
 app.get('/api/status', (request, response) => { Map.onGetStatus(request, response)})
--- src/components/Config.vue
+++ src/components/Config.vue
@@ -1,52 +1,66 @@
 <template>
   <div class="Config" @click="onClickOutside"> 
+
     <div class="Config__backdrop"></div>
+
     <div class="Config__content" @click="onClickInside">
 
-      <h3 class="Config__sectionTitle">Configuration</h3>
+      <div class="Config__form">
+        <h3 class="Config__sectionTitle">Configure your map</h3>
+        <p>Amet eveniet iste explicabo dignissimos dolore.</p>
+        <label for="default_search_location">
+          <strong class="Input__label">Search location</strong>
+          <div class="Input__field Config__field">
+            <input id="default_search_location" type="text" class="Input" placeholder="Default location" v-model="default_search_location">
+          </div>
+        </label>
 
-      <label for="default_search_location">
-        <strong class="Input__label">Search location</strong>
-        <div class="Input__field Config__field">
-          <input id="default_search_location" type="text" class="Input" placeholder="Default location" v-model="default_search_location">
+        <div class="Config__sectionContent">
+          <label for="lon">
+            <strong class="Input__label">Longitude</strong>
+            <div class="Input__field Config__field">
+              <input id="lon" type="text" class="Input" placeholder="Longitude" v-model="lon">
+            </div>
+          </label>
+
+          <label for="lat">
+            <strong class="Input__label">Latitude</strong>
+            <div class="Input__field Config__field">
+              <input id="lat" type="text" class="Input" placeholder="Latitude" v-model="lat">
+            </div>
+          </label>
+
+          <label for="zoom">
+            <strong class="Input__label">Zoom level</strong>
+            <div class="Input__field Config__field is-small">
+              <input id="zoom" type="text" class="Input" placeholder="Zoom" v-model="zoom">
+            </div>
+          </label>
+
         </div>
-      </label>
 
-      <div class="Config__sectionContent">
-        <label for="lon">
-          <strong class="Input__label">Longitude</strong>
-          <div class="Input__field Config__field">
-            <input id="lon" type="text" class="Input" placeholder="Longitude" v-model="lon">
+        <div class="Config__footer">
+          <div class="Config__sectionContent">
+            <div class="Input__field Config__field">
+              <input type="text" class="Input" placeholder="Secret" v-model="secret">
+            </div>
+            <button class="Button is-bold" @click="onClickSaveConfig" :class="saveButtonClass">Save configuration</button>
           </div>
-        </label>
-
-        <label for="lat">
-          <strong class="Input__label">Latitude</strong>
-          <div class="Input__field Config__field">
-            <input id="lat" type="text" class="Input" placeholder="Latitude" v-model="lat">
-          </div>
-        </label>
+        </div>
       </div>
+    </div>
 
-      <label for="zoom">
-        <strong class="Input__label">Zoom level</strong>
-        <div class="Input__field Config__field">
-          <input id="zoom" type="text" class="Input" placeholder="Zoom" v-model="zoom">
-        </div>
-      </label>
+    <div class="Config__content is-dangerous" @click="onClickInside">
 
-
-
-      <button class="Button is-bold" @click="onClickSaveConfig">Save configuration</button>
-
-      <div class="Config__destroy">
+      <div class="Config__footer">
         <h3 class="Config__sectionTitle">Destroy DB</h3>
+        <p>Amet eveniet iste explicabo dignissimos dolore.</p>
         <div class="Config__sectionContent">
           <div class="Input__field Config__field">
-            <input type="text" class="Input" placeholder="Secret" v-model="secret">
+            <input type="text" class="Input" placeholder="Secret" v-model="secretDestroy">
           </div>
 
-          <button class="Button is-bold is-destructive" @click="onClickDestroy">Destroy DB</button>
+          <button class="Button is-bold is-destructive" @click="onClickDestroy" :class="destroyButtonClass">Destroy DB</button>
         </div>
       </div>
 
@@ -61,13 +75,44 @@ import mapConfig from '../../map.yaml'
 
 export default {
   mixins: [mixins],
+  watch: {
+    secretDestroy (val) {
+      if (val) {
+        this.destroyButtonIsEnabled = true
+      } else {
+        this.destroyButtonIsEnabled = false
+      }
+    },
+    secret (val) {
+      if (val) {
+        this.sendButtonIsEnabled = true
+      } else {
+        this.sendButtonIsEnabled = false
+      }
+    }
+  },
   data () {
     return {
+      sendButtonIsEnabled: false,
+      destroyButtonIsEnabled: false,
       default_search_location: mapConfig.DEFAULT_SEARCH_LOCATION,
       lat: mapConfig.LAT,
       lon: mapConfig.LON,
       zoom: mapConfig.ZOOM,
-      secret: undefined
+      secret: undefined,
+      secretDestroy: undefined
+    }
+  },
+  computed: {
+    destroyButtonClass () {
+      if (!this.destroyButtonIsEnabled) {
+        return 'is-disabled'
+      }
+    },
+    saveButtonClass () {
+      if (!this.sendButtonIsEnabled) {
+        return 'is-disabled'
+      }
     }
   },
   mounted () {
@@ -89,21 +134,24 @@ export default {
 
     onRecreateDB (response) {
       response.json().then((result) => {
-        alert(result.ok)
+        window.bus.$emit(config.ACTIONS.STOP_LOADING)
       })
     },
 
     onClickDestroy () {
-      if (!this.secret) {
+      if (!this.secretDestroy) {
         return
       }
 
-      this.post(config.ENDPOINTS.RECREATE_DB, { secret: this.secret })
+      window.bus.$emit(config.ACTIONS.START_LOADING)
+
+      this.post(config.ENDPOINTS.RECREATE_DB, { secret: this.secretDestroy })
         .then(this.onRecreateDB.bind(this))
         .catch((error) => {
           console.log(error)
         })
     },
+
     onGetConfig (response) {
       response.json().then((result) => {
         this.default_search_location = result.default_search_location
@@ -115,12 +163,19 @@ export default {
 
     onSaveConfig (response) {
       response.json().then((result) => {
+      window.bus.$emit(config.ACTIONS.STOP_LOADING)
         console.log(result);
       })
     },
 
     onClickSaveConfig () {
-      this.post(config.ENDPOINTS.CONFIG, { lat: this.lat, lon: this.lon, zoom: this.zoom, default_search_location: this.default_search_location })
+      if (!this.secret) {
+        return
+      }
+
+      window.bus.$emit(config.ACTIONS.START_LOADING)
+
+      this.post(config.ENDPOINTS.CONFIG, { secret: this.secret, lat: this.lat, lon: this.lon, zoom: this.zoom, default_search_location: this.default_search_location })
         .then(this.onSaveConfig.bind(this))
         .catch((error) => {
           console.log(error)
