502e2c0f13ed4578ed2b8e1de15cf18aa7e1ab81
Javier Arce <javierarce@users.noreply.github.com>
2020-06-08 14:18:58 +0200

    Refresh style of the about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -20,15 +20,18 @@
           <p>Follow the development of the tool, suggest improvements, and report bugs in the <a href="https://github.com/javierarce/map-with-me">GitHub repo</a>.</p>
             </div>
             <div>
-              <div class="About__columnTitle">Copyright</div>
-              <p>This website uses data from <a href="https://www.openstreetmap.org/copyright">Nominatim</a>. If you remix it, please read its <a href="https://operations.osmfoundation.org/policies/nominatim/">usage policy</a>.</p>
+              <div class="About__columnTitle">Download the data</div>
+              <p>
+                The data of this map is available in a <a href="/csv" title="Export locations in CSV format" target="_blank">CSV file</a>.  You can also subscribe to this map using <a href="/rss" title="RSS" target="_blank">RSS</a>.
+              </p>
+
             </div>
           </div>
 
         </div>
         <div class="About__footer">
           <div class="About__export">
-            <a href="/rss" title="RSS" target="_blank">RSS</a> &middot; <a href="/csv" title="Export locations in CSV format" target="_blank">Download locations</a> (.CSV)
+              <p>This website uses data from <a href="https://www.openstreetmap.org/copyright">Nominatim</a>. If you remix it, please read its <a href="https://operations.osmfoundation.org/policies/nominatim/">usage policy</a>.</p>
           </div>
           <div class="About__copyright">Made by <a href="https://twitter.com/javier">Javier Arce</a></div>
         </div>
