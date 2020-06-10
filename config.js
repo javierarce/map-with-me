ab03ee900ed0cc287516e889bd683d88bdef3a36
Javier Arce <javierarce@users.noreply.github.com>
2020-06-10 09:13:02 +0200

    Improves text

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const TEXTS = {
   PLACEHOLDER: 'What\'s cool about this place?',
-  SEARCH_PLACEHOLDER: 'Search for a cool place / address',
-  NO_RESULTS_TITLE:  'Oh, no, we couldn’t find "{q}" :(',
+  SEARCH_PLACEHOLDER: 'Search for a place or address',
+  NO_RESULTS_TITLE:  'Oh, no… we couldn’t find "{q}."',
   NO_RESULTS_DESCRIPTION: `<p><strong>Map with Me</strong> uses data from <a href="https://www.openstreetmap.org">OpenStreetMap</a> 
-  (OSM), a collaborative project which aims to create a free editable map of the world and that place hasn’t been added yet.</p>
-  <p>But the good news is that you can add it to OSM yourself and help improve a free and open map of the world! Just visit <a href="https://www.openstreetmap.org">OSM</a>, zoom to an area, and click "edit". (You'll need an account first). The editor has a built-in guide that will explain you the basics.</p>`,
+  (a collaborative project which aims to create a free editable map of the world) and that place hasn’t been added yet.</p>
+  <p>But the good news is that you can add it to OSM yourself and help improve a free and open map of the world! Visit <a href="https://www.openstreetmap.org">OSM</a>, zoom to an area, and click "edit".</p>`,
 }
 
 const ACTIONS = {
